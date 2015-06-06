fe4408a1c4eb9ad8a5430c8f34c3a52ef76f836e
devBeginner <name.nachname2@gmail.com>
2015-06-06 11:16:15 +0200

    rest

---
+++ P2_3.cs
@@ -0,0 +1,291 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using GLab.Core;
+using GLab.Core.Forms;
+using GLab.Core.PluginInterfaces;
+using GLab.Chaos;
+using Microsoft.Xna.Framework;
+using Color = System.Drawing.Color;
+using GLab.Core.Forms.Input;
+using GLab.Chaos.Datastructure;
+#endregion
+
+namespace Frame.Chaos
+{
+
+    /// <summary>
+    ///   Your plugin description.
+    /// </summary>
+    internal class P2_3 : IPluginClient, IGuiExtension
+    {
+        /// <summary>
+        ///   Initial size of the raster image.
+        /// </summary>
+        private const int ImageWidth = 512, ImageHeight = 512;
+
+        private FrmImage _frame;
+        private Image<Rgb, byte> _image;
+        private Random _rand;
+
+        private Point[] _initPoints = new Point[5];
+        private Painter _painter;
+        private IteratedFunctionSystem _ifs;
+
+        private const int MAX_COORDINATE = 1024;
+        private CoordinateSystem _coord;
+
+        private double[] arrProbs = new double[10];
+
+        private int iKlicks = 0;
+
+        public P2_3()
+        {
+            Name = "P2 Aufgabe 3";
+        }
+
+        #region IGuiExtension Members
+
+        public void AddMenuStripItems(MenuStrip menuStrip)
+        {
+            // Add items to the menu here
+        }
+
+        public void AddToolStripItems(ToolStrip toolStrip)
+        {
+            // Add items to the toolbar here
+        }
+
+        public void RemoveMenuStripItems(MenuStrip menuStrip)
+        {
+            // Remove all items from the menu
+        }
+
+        public void RemoveToolStripItems(ToolStrip toolStrip)
+        {
+            // Remove all items from the toolbar
+        }
+
+        #endregion
+
+        public override void Setup()
+        {
+            _coord = new CoordinateSystem(-1, 1, -1, 1);
+
+            // Init. a new random number generator
+            _rand = new Random();
+
+            // Create a new raster image for drawing
+            _image = new Image<Rgb, byte>(MAX_COORDINATE, MAX_COORDINATE, new Rgb(Color.Black));
+
+            // Painter initialisieren mit neuem Koordinatensystem
+            _painter = new Painter(ref _image, _coord);
+
+            // Create a new frame to display the raster image
+            _frame = new FrmImage(Name, MAX_COORDINATE, MAX_COORDINATE, DisplayMode.Zoomable)
+                {
+                    InterpolationMode = InterpolationMode.NearestNeighbor,
+                    SmoothingMode = SmoothingMode.None
+                };
+            _frame.SetImage(_image);
+
+            // Register the click event handler
+            _frame.PictureBox.MouseClick += drawAufgabeFromDot;
+
+            // Register this plugin as a GUI extension
+            GLabController.Instance.RegisterExtension(this);
+
+        }
+
+        // 
+        private void Aufgabe(Vector3 vecAlt)
+        {
+
+            // Startpunkt in der Mitte des Koordinatensystem
+            Color colorPaint = GenerateRandomColor();
+            int i = 0;
+
+            do
+            {
+                // Punkt zeichnen
+                _painter.PaintPoint(vecAlt, colorPaint);
+
+                // neue Zufallsfarbe
+                if (i % 500 == 0)
+                {
+                    colorPaint = GenerateRandomColor();
+                    //colorPaint = Color.Green;
+                    _frame.Repaint();
+                }
+
+                // Zufälliges Matrix anwenden
+                Vector3 vecNeu = Vector3.Transform(vecAlt, getRandTransfMatrix());
+
+                vecAlt = vecNeu;
+
+                i++;
+
+            } while (i < 100000);
+
+            _frame.Refresh();
+
+        }
+
+        private Microsoft.Xna.Framework.Matrix getRandTransfMatrix()
+        {
+            double fProb = _rand.NextDouble();
+            double dSum = arrProbs[1];
+            int iIndex = 0;
+
+            // Index mithilfe der Wahrscheinlichkeiten ermitteln
+
+            while (fProb > dSum)
+            {
+                iIndex++;
+                dSum += arrProbs[iIndex + 1];
+            }
+
+            return _ifs[iIndex];
+        }
+
+        private void readIfsProbabilities()
+        {
+            double fDeter = 0;
+
+            // Array zurücksetzen
+            for (int i = 0; i < arrProbs.Length; i++)
+            {
+                arrProbs[i] = 0;
+            }
+
+            // Alle Determinanten ermitteln und summe berechnen
+            for (int i = 0; i < _ifs.Count; i++)
+            {
+                fDeter = calcDeterminant(_ifs[i]);
+                arrProbs[0] += fDeter;
+                arrProbs[i + 1] = fDeter;
+            }
+
+            // Prozentwerte berechnen
+            for (int i = 1; i < arrProbs.Length; i++)
+            {
+                // Determinante geteilt durch die Summe der Determinanten
+                arrProbs[i] /= arrProbs[0];
+            }
+
+        }
+
+        private void drawAufgabeFromDot(object sender, MouseEventArgs e)
+        {
+            float iX, iY;
+            string strIFS = "";
+
+            iX = (e.X * (_coord.MaxX - _coord.MinX)) / (float)ImageWidth + _coord.MinX;
+            iY = (e.Y * (_coord.MaxY - _coord.MinY)) / (float)ImageHeight + _coord.MinY;
+
+            // IFS laden
+            switch (iKlicks)
+            {
+                case 0:
+                    strIFS = "Farn_1.IFS";
+                    break;
+                case 1:
+                    strIFS = "Bigbang.IFS";
+                    break;
+                case 2:
+                    strIFS = "Filmstreifen.IFS";
+                    break;
+                case 3:
+                    strIFS = "Strauch.IFS";
+                    break;
+                case 4:
+                    strIFS = "Swirl.IFS";
+                    break;
+                case 5:
+                    strIFS = "Wirbel_blatt.IFS";
+                    break;
+                case 6:
+                    Logger.Instance.LogInfo("\n" + "!!! Jetzt kommt Aufgabe 4 !!!");
+                    strIFS = "IFS_TEST.IFS";
+                    break;
+                default:
+                    strIFS = "Farn_1.IFS";
+                    iKlicks = 0;
+                    break;
+            }
+            iKlicks++;
+            _ifs = GLabReader.ReadIfsFromFile("..\\..\\IFS2\\" + strIFS);
+            Logger.Instance.LogInfo("IFS:\t" + strIFS);
+            readIfsProbabilities();
+
+            // Create a new raster image for drawing
+            _image = new Image<Rgb, byte>(MAX_COORDINATE, MAX_COORDINATE, new Rgb(Color.Black));
+
+            // Painter initialisieren mit neuem Koordinatensystem
+            _coord = new CoordinateSystem(_ifs.CoordinateSystem.MinX, _ifs.CoordinateSystem.MaxX, _ifs.CoordinateSystem.MinY, _ifs.CoordinateSystem.MaxY);
+            _painter = new Painter(ref _image, _coord);
+            _frame.SetImage(_image);
+            _painter.PaintCoordinateSystem(16, Color.Red);
+
+            // Punkt zeichnen
+            Aufgabe(getFixPoint());
+
+        }
+
+        private Vector3 getFixPoint()
+        {
+
+            float dX = 0;
+            float dY = 0;
+            float a = _ifs[0].M11;
+            float b = _ifs[0].M21;
+            float c = _ifs[0].M12;
+            float d = _ifs[0].M22;
+            float e = _ifs[0].M31;
+            float f = _ifs[0].M32;
+
+            dX = (-e * (d - 1) + b * f) / ((a - 1) * (d - 1) - b * c);
+            dY = (-f * (a - 1) + c * e) / ((a - 1) * (d - 1) - b * c);
+
+            return new Vector3(dX, dY, 1);
+
+        }
+
+        private double calcDeterminant(Microsoft.Xna.Framework.Matrix ifs_transf)
+        {
+            double dPerc = ifs_transf.M11 * ifs_transf.M22 - ifs_transf.M21 * ifs_transf.M12;
+
+            if (dPerc == 0)
+            {
+                dPerc = 0.01d;
+            }
+
+            return Math.Abs(dPerc);
+        }
+
+        private System.Drawing.Color GenerateRandomColor()
+        {
+            return System.Drawing.Color.FromArgb(_rand.Next(256), _rand.Next(256), _rand.Next(256));
+        }
+
+        public override void Run()
+        {
+            // Executed in each iteration of the render loop
+        }
+
+        public override void Teardown()
+        {
+            GLabController.Instance.UnregisterExtension(this);
+            _frame.Dispose();
+            _image.Dispose();
+
+            // Additional teardown of your plugin
+            // ...
+        }
+    }
+}
---
+++ P2_6.cs
@@ -0,0 +1,236 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using GLab.Core;
+using GLab.Core.Forms;
+using GLab.Core.PluginInterfaces;
+using GLab.Chaos;
+using Microsoft.Xna.Framework;
+using Color = System.Drawing.Color;
+using GLab.Core.Forms.Input;
+using GLab.Chaos.Datastructure;
+using GLab.Core.Forms.Control;
+#endregion
+
+namespace Frame.Chaos
+{
+
+
+    /// <summary>
+    ///   Your plugin description.
+    /// </summary>
+    internal class P2_6 : IPluginClient, IGuiExtension
+    {
+        public struct Ant
+        {
+            public int iZustand;
+            public int iCurrX;
+            public int iCurrY;
+            public int iDirec;
+
+        }
+        /// <summary>
+        ///   Initial size of the raster image.
+        /// </summary>
+        private const int ImageWidth = 501, ImageHeight = 501;
+
+        private FrmImage _frame;
+        private Image<Rgb, byte> _image;
+        private Random _rand;
+
+        private Point[] _initPoints = new Point[5];
+        private Painter _painter;
+
+        private const int MAX_COORDINATE = 101;
+        private CoordinateSystem _coord;
+
+        private double[] arrProbs = new double[10];
+        
+        FrmFlowControl flow = new FrmFlowControl();
+
+        private String strAnt;
+        private Palette palAnt;
+
+        private Vector2[] vecDirec = { new Vector2(0, 1), new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, 0) };
+        private int[] arrRot = { -1, 1 };
+        private int[,] arrZustand = new int[ImageWidth, ImageHeight];
+        private int AnzahlZustaende;
+
+        Ant ant1 = new Ant();
+        Ant ant2 = new Ant();
+
+        private string[] arrAnts = {
+            "..\\..\\Ants\\Ant_0.ant",
+            "..\\..\\Ants\\Ant_1.ant",
+            "..\\..\\Ants\\Ant_2.ant",
+            "..\\..\\Ants\\Ant_3.ant",
+            "..\\..\\Ants\\Ant_4.ant",
+            "..\\..\\Ants\\Ant_5.ant",
+            "..\\..\\Ants\\Ant_6.ant",
+            "..\\..\\Ants\\Ant_7.ant",
+            "..\\..\\Ants\\Ant_8.ant",
+            "..\\..\\Ants\\Ant_9.ant",
+            "..\\..\\Ants\\Ant_10.ant",
+            "..\\..\\Ants\\Ant_11.ant",
+            "..\\..\\Ants\\Ant_12.ant",
+            "..\\..\\Ants\\Ant_13.ant",
+            "..\\..\\Ants\\Ant_14.ant"};
+
+        public P2_6()
+        {
+            Name = "P2 Aufgabe 6";
+            ant1.iZustand = -1;
+            ant2.iZustand = -1;
+        }
+
+        #region IGuiExtension Members
+
+        public void AddMenuStripItems(MenuStrip menuStrip)
+        {
+            // Add items to the menu here
+        }
+
+        public void AddToolStripItems(ToolStrip toolStrip)
+        {
+            // Add items to the toolbar here
+        }
+
+        public void RemoveMenuStripItems(MenuStrip menuStrip)
+        {
+            // Remove all items from the menu
+        }
+
+        public void RemoveToolStripItems(ToolStrip toolStrip)
+        {
+            // Remove all items from the toolbar
+        }
+
+        #endregion
+
+        public override void Setup()
+        {
+            _coord = new CoordinateSystem(-0, ImageWidth, 0, ImageHeight);
+
+            // Init. a new random number generator
+            _rand = new Random();
+
+            // Create a new raster image for drawing
+            _image = new Image<Rgb, byte>(ImageWidth, ImageHeight, new Rgb(Color.White));
+
+            // Painter initialisieren mit neuem Koordinatensystem
+            _painter = new Painter(ref _image);
+
+            // Create a new frame to display the raster image
+            _frame = new FrmImage(Name, ImageWidth + 1, ImageHeight + 1, DisplayMode.Zoomable)
+                {
+                    InterpolationMode = InterpolationMode.NearestNeighbor,
+                    SmoothingMode = SmoothingMode.None
+                };
+
+            _frame.SetImage(_image);
+
+            // Register the click event handler
+            _frame.PictureBox.MouseClick += addAnt2;
+
+            // Register this plugin as a GUI extension
+            GLabController.Instance.RegisterExtension(this);
+
+            palAnt = GLabReader.ReadPaletteFromFile("..\\..\\Ants\\Chaos_ant.pal");
+
+            Aufgabe();
+
+        }
+
+        private void addAnt2(object sender, MouseEventArgs e)
+        {
+            if (ant2.iZustand < 0)
+            {
+                ant2.iZustand = 0;
+                ant2.iCurrX = e.X;
+                ant2.iCurrY = e.Y;
+            }
+        }
+        
+        private Ant doStep(Ant ant)
+        {
+            if (ant.iZustand < 0) return ant;
+
+            // Alten und neuen Zustand an der aktuellen stelle ermitteln
+            int zustAlt = arrZustand[ant.iCurrX, ant.iCurrY];
+            int zustNeu = (zustAlt + 1) % AnzahlZustaende;
+            Color colNew = palAnt[zustNeu];
+
+            // neue Farbe an der stelle setzen und Zustand anpassen
+            _image[ant.iCurrY, ant.iCurrX] = new Rgb(colNew.R, colNew.G, colNew.B);
+            arrZustand[ant.iCurrX, ant.iCurrY] = zustNeu;
+
+            // Prüfen, welche rotation an dem entsprechenden Zustand durchgeführt werden muss
+            // und dann die entspechende Richtung auswählen
+            int iRot = Convert.ToInt32(strAnt.Substring((AnzahlZustaende - 1) - zustNeu, 1));
+            ant.iDirec += vecDirec.Length + arrRot[iRot];
+            ant.iDirec %= vecDirec.Length;
+
+            // Einen Schritt in die neue Richtung machen
+            ant.iCurrX = (ant.iCurrX + (int)vecDirec[ant.iDirec].X + ImageWidth) % ImageWidth;
+            ant.iCurrY = (ant.iCurrY + (int)vecDirec[ant.iDirec].Y + ImageHeight) % ImageHeight;
+
+            return ant;
+        }
+
+        // wenn aktuelles feld weiß ist, dann rot färben - nach links drehen und eins vorwärts
+        // wenn aktuelles feld rot, dann weiß färben - nach rechts drehen und eins vorwärts
+        private void Aufgabe()
+        {            
+          
+            flow.StepCount = 100;
+
+            for (int iAnt = 0; iAnt < arrAnts.Length; iAnt++)
+            {
+                strAnt = GLabReader.ReadAntFromFile(arrAnts[iAnt]);
+                AnzahlZustaende = strAnt.Length;
+                arrZustand = new int[ImageWidth, ImageHeight];
+                _image = new Image<Rgb, byte>(ImageWidth, ImageHeight, new Rgb(Color.White));
+                _frame.SetImage(_image);
+                ant1.iZustand = 0;
+                ant1.iCurrX = (ImageWidth - 1) / 2;
+                ant1.iCurrY = (ImageHeight - 1) / 2;
+
+                for (int i = 0; i < 1000000; i++)
+                {
+                    if (i % 2000 == 0)
+                    {
+                        _frame.Repaint();
+                        Logger.Instance.LogInfo(""+i);
+                    }
+
+                    //for (Int64 ix = 0; ix < 10000; ix++) { }
+                    flow.Wait();
+                    ant1 = doStep(ant1);
+                    ant2 = doStep(ant2);
+
+                }
+            }
+
+        }
+
+        public override void Run()
+        {
+            // Executed in each iteration of the render loop
+        }
+
+        public override void Teardown()
+        {
+            GLabController.Instance.UnregisterExtension(this);
+            _frame.Dispose();
+            _image.Dispose();
+
+            // Additional teardown of your plugin
+            // ...
+        }
+    }
+}
