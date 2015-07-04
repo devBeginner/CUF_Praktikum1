d8763edc6dfb667c2cd22fd0d80feea545ed5b90
devBeginner <name.nachname2@gmail.com>
2015-07-04 14:14:49 +0200

    Final

---
+++ P3_A1.cs
@@ -0,0 +1,319 @@
+﻿#region
+
+using System;
+using GLab.Core;
+using GLab.Core.PluginInterfaces;
+using GLab.Rendering;
+using GLab.Rendering.Camera;
+using GLab.Rendering.Primitives;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using GLab.Chaos;
+using GLab.Chaos.Datastructure;
+using GLab.Core.Forms.Input;
+using GLab.Core.Forms.Control;
+
+#endregion
+
+namespace GLab.Example.Chaos
+{
+    /// <summary>
+    ///   Creating a scene with different types of cameras and some basic primitives.
+    /// </summary>
+    class P3_A1 : IPluginClient
+    {
+        private RenderView _flyCamView;
+
+        private ContentManager _contentManager;
+
+        private FlyCamera _flyCamera;
+        //private RenderView _polarCamView;
+        //private PolarCamera _polarCamera;
+
+        private Scene _scene;
+
+        private MatrixStack _matrixStack;
+        private PlatonicSolidFactory _psf;
+        private LindenmayerSystem _lindenmayerSystem;
+        private float _size;
+        private float _width;
+
+        private Random _rand;
+
+        private Vector3 _vecStart;
+        private Vector3 _pos;
+
+        FrmFlowControl flow;
+        string filename = "";
+        string currentAxiom = "";
+
+        /// <summary>
+        ///   Init. the example.
+        /// </summary>
+        public P3_A1()
+        {
+            Name = "Praktikum 3";
+
+        }
+
+        /// <summary>
+        ///   Setup the example.
+        /// </summary>
+        public override void Setup()
+        {
+            _scene = new Scene();
+            _rand = new Random();
+            _contentManager = new ContentManager(XnaRenderer.Instance.Services,
+                                     GLabController.Instance.HomeDirectory + "\\Data\\FractalTerrain");
+
+            _psf = new PlatonicSolidFactory(XnaRenderer.Instance);
+            _matrixStack = new MatrixStack();
+            filename = InputHelper.LoadFileDialog("Load LIN-File");
+            _lindenmayerSystem = GLabReader.ReadLindenmayerSystemFromFile(filename);
+
+            if (filename.Contains("1_")) { _pos = new Vector3(0, 0, Math.Abs(_lindenmayerSystem.CoordinateSystem.MaxX) + Math.Abs(_lindenmayerSystem.CoordinateSystem.MinX)); }
+            if (filename.Contains("2_")) { _pos = new Vector3(0, 0, 3); }
+            if (filename.Contains("3_")) { _pos = new Vector3(5, 5, 5); }
+
+            flow = new FrmFlowControl();
+
+            // Breite und Länge initialisieren
+            _vecStart = new Vector3(0, 0, 0);
+
+            Vector3 pos = _pos; // new Vector3(0, 0, 5);
+            Vector3 lookAt = Vector3.Zero;
+            Vector3 up = Vector3.UnitY;
+
+
+            //_polarCamera = new PolarCamera();
+            //_polarCamera.SetLookAt(pos, lookAt, up);
+            //_polarCamera.ActivateControl();
+            //_polarCamView = new RenderView(512, 512, _scene, _polarCamera,
+            //                               "Polar Camera");
+
+            //Logger.Instance.LogInfo(
+            //    "Controls (Polar Camera): <W>, <A>, <S>, <D>, <Q>, <E> and mouse movement (while left or right clicking).");
+
+            _flyCamera = new FlyCamera();
+            _flyCamera.ActivateControl();
+            _flyCamera.SetLookAt(pos, lookAt, up);
+            _flyCamView = new RenderView(256, 256, _scene, _flyCamera, "Fly Camera");
+
+            Logger.Instance.LogInfo(
+                "Controls (Fly Camera): <W>, <A>, <S>, <D>, <Q>, <E> and mouse movement (while left clicking). Use <R> to reset the camera to the initial position.");
+
+            //XnaRenderer.Instance.Views.Add(_polarCamView);
+            XnaRenderer.Instance.Views.Add(_flyCamView);
+
+            Start();
+            _scene.RenderCoordinateAxes = false;
+            flow.StepCount = 1;
+            currentAxiom = _lindenmayerSystem.Axiom;
+            int iStep = 11;
+            while (true)
+            {
+                iStep++;
+                nextGeneration(_scene, 0, iStep, currentAxiom);
+                flow.Wait();
+                Logger.Instance.LogInfo("" + iStep);
+            }
+
+        }
+
+
+        private void nextGeneration(Scene _scene, int iCurRec, int iMaxRec, string strAxiom)
+        {
+            GeometricPrimitive gp = null;
+            string _currentString = strAxiom;
+            float changeAngle = (float)_lindenmayerSystem.ChangeOfAngle;
+            _matrixStack = new MatrixStack();
+            const float DEF_WIDTH = 0.25f;
+
+            if (_lindenmayerSystem.GrSwitch == true)
+            {
+                // Matrixstack initialisieren
+                _scene.Renderables.Clear();         // Szene zurücksetzen
+            }
+
+            // Identität laden und in die korrekte Ausgangsrichtung bringen
+            _matrixStack.LoadIdentity();
+            _matrixStack.Rotate(_lindenmayerSystem.InitialAngle, new Vector3(0, 0, 1));
+            _matrixStack.PushMatrix();
+
+            // breite und länge für die rekursionsstufe setzen
+            _width = DEF_WIDTH;// DEF_WIDTH;// * _lindenmayerSystem.ScalePerIteration;// *(float)Math.Pow(_lindenmayerSystem.ScalePerIteration, iCurRec);
+            _size = (float)Math.Pow(_lindenmayerSystem.ScalePerIteration, iCurRec);
+
+
+            if (filename.Contains("_3d"))
+                _width = _size;
+            if (filename.Contains("Sierpinsk"))
+                _width = 1f;
+            if (filename.Contains("3_Hilbert"))
+                _width = 0.5f;
+
+
+            System.Drawing.Color coltmp = GenerateRandomColor();
+            Color currentColor = new Color(coltmp.R, coltmp.G, coltmp.B);
+
+            // Interpretiere den aktuellen String _currentString Zeichen für Zeichen
+            for (int i = 0; i < _currentString.Length; i++)
+            {
+
+                switch (_currentString[i])
+                {
+
+                    case 'X':
+                    case 'Y':
+                    case 'G':
+                    case 'F':
+
+                        _matrixStack.PushMatrix();
+
+
+                        _matrixStack.Translate(new Vector3(_size / 2, 0, 0));
+                        _matrixStack.Scale(new Vector3(_size, _size * _width, _size * _width));
+
+                        gp = _psf.CreateGeometricPrimitive(PlatonicSolid.Hexahedron, currentColor, _matrixStack.Transform);
+
+                        _scene.Renderables.Add(gp);
+
+                        _matrixStack.PopMatrix();
+
+                        _matrixStack.Translate(new Vector3(_size, 0, 0));
+
+                        //flow.Wait();
+
+                        break;
+
+                    case 'g':
+                    case 'f':
+                        _matrixStack.Translate(new Vector3(_size, 0, 0));
+                        break;
+
+                    case '-':
+                        _matrixStack.Rotate(-changeAngle, 0, 0, 1); // rechts-Drehung in x-y-Ebene
+                        break;
+
+                    case '+':
+                        _matrixStack.Rotate(changeAngle, 0, 0, 1); // Links-Drehung in x-y-Ebene
+                        break;
+
+                    case '&':
+                        _matrixStack.Rotate(-changeAngle, 0, 1, 0); // rechts-Drehung in x-y-Ebene
+                        break;
+
+                    case '^':
+                        _matrixStack.Rotate(changeAngle, 0, 1, 0); // Links-Drehung in x-y-Ebene
+                        break;
+
+                    case '*':
+                        _matrixStack.Rotate(-changeAngle, 1, 0, 0); // rechts-Drehung in x-y-Ebene
+                        break;
+
+                    case '/':
+                        _matrixStack.Rotate(changeAngle, 1, 0, 0); // Links-Drehung in x-y-Ebene
+                        break;
+
+                    case '|':
+                        _matrixStack.Rotate(180, 0, 0, 1);
+                        _matrixStack.Rotate(180, 0, 1, 0);
+                        _matrixStack.Rotate(180, 1, 0, 0);
+                        break;
+
+                    case '[':
+                        _matrixStack.PushMatrix();
+                        if (filename.Contains("Baum_3") || filename.Contains("Busch_3"))
+                            _width = _width * _lindenmayerSystem.ScalePerIteration;
+                        //_size *= _lindenmayerSystem.ScalePerIteration;
+                        break;
+
+                    case ']':
+                        _matrixStack.PopMatrix();
+                        if (filename.Contains("Baum_3") || filename.Contains("Busch_3"))
+                            _width = _width / _lindenmayerSystem.ScalePerIteration;
+                        //_size /= _lindenmayerSystem.ScalePerIteration;
+                        break;
+
+                } // end of switch
+            }
+            //_scene.Renderables.Add(gp);
+            //_matrixStack.PopMatrix();
+
+            flow.Wait();
+
+            // Wenn max Iteration
+            if (iCurRec == iMaxRec)
+            {
+                if (!filename.Contains("2_Koch"))
+                {
+                    currentAxiom = _lindenmayerSystem.Axiom;
+                }
+                return;
+            }
+            else
+            {
+
+                iCurRec++;
+
+                if (filename.Contains("2_Koch"))
+                {
+                    currentAxiom = "";
+                    for (int i = 0; i < _currentString.Length; i++)
+                    {
+                        if (_currentString[i] == 'F')
+                        {
+                            if (_rand.Next() % 2 == 0)
+                            {
+
+                                currentAxiom += "F-F++F-F";
+                            }
+                            else
+                            {
+                                currentAxiom += "F+F--F+F";
+
+                            }
+
+                        }
+                        else
+                        {
+                            currentAxiom += _currentString[i];
+                        }
+                    }
+                }
+                else
+                {
+                    currentAxiom = _lindenmayerSystem.ComposeStringFromOtherString(currentAxiom, 1);
+                }
+
+                nextGeneration(_scene, iCurRec, iMaxRec, currentAxiom);
+            }
+
+        }
+
+        private System.Drawing.Color GenerateRandomColor()
+        {
+            return System.Drawing.Color.FromArgb(_rand.Next(256), _rand.Next(256), _rand.Next(256));
+        }
+
+        /// <summary>
+        ///   Executed once in each of the iterations of the render loop.
+        /// </summary>
+        public override void Run()
+        {
+            XnaRenderer.Instance.RenderFrame();
+        }
+
+        /// <summary>
+        ///   Cleans up any created resources and stops the render loop.
+        /// </summary>
+        public override void Teardown()
+        {
+            Stop();
+            //_polarCamView.Close();
+            _flyCamView.Close();
+            XnaRenderer.Instance.Views.Clear();
+        }
+    }
+}
---
+++ P3_A4.cs
@@ -0,0 +1,495 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using GLab.Core;
+using GLab.Core.Forms;
+using GLab.Core.PluginInterfaces;
+using GLab.Chaos;
+using Microsoft.Xna.Framework;
+using Color = System.Drawing.Color;
+using GLab.Core.Forms.Input;
+using GLab.Chaos.Datastructure;
+using GLab.Core.Forms.Control;
+#endregion
+
+namespace Frame.Chaos
+{
+
+
+    /// <summary>
+    ///   Your plugin description.
+    /// </summary>
+    internal class P3_4 : IPluginClient, IGuiExtension
+    {
+        // zum speichern der Stati
+        public enum eState { empty, wood, termite }
+
+        private FrmImage _frame;
+        private Image<Rgb, byte> _image;
+        private Random _rand = new Random();
+
+
+        private const int MAX_TERMITE = 3000;
+        private const int MAX_WOOD = 5000;
+        private const int ImageWidth = 256, ImageHeight = 256;
+
+        // Zum speichern der Zustände der einzelnen Pixel (leer, termite oder holz)
+        private eState[,] state = new eState[ImageWidth, ImageHeight];
+
+        // Array mit den Termiten, um sie in jeder Runde einfach weiterlaufen zu lassen
+        private Termite[] termites = new Termite[MAX_TERMITE];
+
+        // Array mit den verschiedenen Richtungen, der jeweils nächste Eintrag 
+        // entspricht einer Rotation um 45° nach recht.
+        private static Vector2[] arrDirection = { 
+            new Vector2(0, 1), 
+            new Vector2(1, 1), 
+            new Vector2(1, 0), 
+            new Vector2(1, -1), 
+            new Vector2(0, -1), 
+            new Vector2(-1, -1), 
+            new Vector2(-1, 0),
+            new Vector2(-1, 1) 
+        };
+
+        // Array zum ändern der Richtung links vor, gerade aus, oder rechts vor
+        // das gerade ausgehen ist wahrscheinlicher (3x0)
+        private static int[] arrChDir = { -1, 0, 0, 0, 1 };
+
+        public P3_4()
+        {
+            Name = "Termiten";
+        }
+
+        // Die Termite 
+        internal class Termite
+        {
+            // Aktuelle Position únd Richtung
+            public int X;
+            public int Y;
+            public int Dir;
+
+            // Die Position für die nächste zufällige Laufrichtung
+            private int Xnext;
+            private int Ynext;
+            private int DirNext;
+
+            // Zufallszahl initialisieren
+            private Random _rand = new Random();
+
+            // hat die Termite einen Chip
+            public bool hasChip = false;
+
+            // kann sie sich nicht bewegen, weil alle Richtungen blockiert sind
+            public bool stand = false;
+
+            // Anzahl der Runden, bevor sie wieder einen Holzchip aufnehmen darf
+            public int countDown = 0;
+
+            public Termite(eState[,] state)
+            {
+                getRandPosition(state);
+                Dir = _rand.Next() % arrDirection.Length;
+                state[X, Y] = eState.termite;
+                hasChip = false;
+                countDown = 0;
+            }
+
+            // Zufällige nächste Position ermitteln
+            private void loadNextPosition(eState[,] state)
+            {
+                int count = -1;
+
+                // Wenn die Termite erst einen Chip abgelegt hat, dann in Zufällige Richtung laufen, an der eine Freie stelle ist
+                if (countDown > 0)
+                {
+                    // Solange versuchen eine zufälltige Richtung zu ermitteln, bis alle probiert wurden oder ein freies 
+                    // Feld gefunden wurde
+                    do
+                    {
+                        // Zufällig Richtung ermitteln und 
+                        DirNext = _rand.Next() % arrDirection.Length;
+
+                        // Neue Position der Termite laden
+                        Xnext = (X + (int)arrDirection[DirNext].X + ImageWidth) % ImageWidth;
+                        Ynext = (Y + (int)arrDirection[DirNext].Y + ImageHeight) % ImageHeight;
+
+                        count++;
+
+                        // Wenn keine freie Stelle gefunden wurde und noch nicht alle Richtungen durhclaufen wurden
+                    } while (count < arrDirection.Length && state[Xnext, Ynext] != eState.empty);
+
+                    // ein Schritt weniger, bis die Termite wieder einen Holzchip aufnehmen kann
+                    countDown--;
+
+                }
+                // zufällige Richtung nach vorne (rechts/links)
+                else
+                {
+                    // Solange versuchen, eine neue Position zu ermitteln, 
+                    // bis eine gefunden wurde, an der keine andere Termite sitzt
+                    // oder aber alle Richtungen durchprobiert wurden und keine gefunden wurde 
+                    do
+                    {
+                        // Zufällig nach vorne, links oder rechts drehen
+                        DirNext = Dir + arrDirection.Length + arrChDir[_rand.Next() % arrChDir.Length];
+
+                        // Index ermitteln
+                        DirNext %= arrDirection.Length;
+
+                        // Neue Position der Termite laden
+                        Xnext = (X + (int)arrDirection[DirNext].X + ImageWidth) % ImageWidth;
+                        Ynext = (Y + (int)arrDirection[DirNext].Y + ImageHeight) % ImageHeight;
+
+                        count++;
+
+                    } while (count < arrDirection.Length && state[Xnext, Ynext] == eState.termite);
+
+
+                }
+
+                // wenn keine Stelle zum hinlaufen gefunden wurde, stehen bleiben und umdrehen
+                // sodass sie in der nächsten Runde in die entgegengesetzte Richtung versucht
+                // zu laufen
+                if (count == arrDirection.Length)
+                {
+                    Dir += arrDirection.Length / 2;
+                    Dir %= arrDirection.Length;
+
+                    stand = true;
+
+                }
+                else
+                {
+                    stand = false;
+                }
+
+            }
+
+            //setz den chip an der Stelle und gibt zurück ob es erfolgreich war
+            private bool setWood(eState[,] state)
+            {
+
+                bool set = false;
+                int randDir = 0;
+                int x;
+                int y;
+
+                if (state[X, Y] == eState.wood)
+                {
+                    return false;
+                }
+
+                // Holz an die aktuelle Stelle, oder eins rechts oder links setzen
+                do
+                {
+                    randDir = arrChDir[_rand.Next() % arrChDir.Length];
+
+                    if (randDir != 0)
+                    {
+                        randDir += DirNext + arrDirection.Length;
+                        randDir %= arrDirection.Length;
+
+                        // Neue Position zum hinlegen laden
+                        x = (X + (int)arrDirection[randDir].X + ImageWidth) % ImageWidth;
+                        y = (Y + (int)arrDirection[randDir].Y + ImageHeight) % ImageHeight;
+                    }
+                    else
+                    {
+                        x = X;
+                        y = Y;
+                    }
+
+                    // wenn an der Stelle nichts ist
+                    if (state[x, y] == eState.empty || (x == X && y == Y))
+                    {
+                        // Holz setzen an die aktuelle Stelle
+                        state[X, Y] = eState.wood;
+
+                        // merken, dass sie den Chip weggelegt hat
+                        hasChip = false;
+
+                        set = true;
+
+                    }
+
+                } while (set == false);
+
+                // Zurückgeben, dass es gesetzt wurde
+                return true;
+
+            }
+
+            // gibt zurück, ob dort ein Chip lag und setzt die stelle auf false
+            private bool pickWood(eState[,] state)
+            {
+                if (state[Xnext, Ynext] != eState.wood)
+                {
+                    return false;
+                }
+                else
+                {
+                    // Status an der aktuellen Stelle entfernen und 
+                    // Termite auf die neue Stelle setzen
+                    state[X, Y] = eState.empty;
+                    state[Xnext, Ynext] = eState.termite;
+
+                    // merken, dass die Termite einen Chip hat
+                    this.hasChip = true;
+
+                    return true;
+                }
+            }
+
+            // setzt die Termite irgendwohin, wo kein Holz ist
+            private void getRandPosition(eState[,] state)
+            {
+                do
+                {
+                    X = _rand.Next() % ImageWidth;
+                    Y = _rand.Next() % ImageHeight;
+
+                } while (state[X, Y] != eState.empty);
+            }
+
+            // An die neue berechnete Stelle gehen
+            private void move(eState[,] state)
+            {
+                X = Xnext;
+                Y = Ynext;
+                Dir = DirNext;
+            }
+
+            // einen Schritt machen
+            internal void doStep(eState[,] state)
+            {
+                eState nextState;
+
+                // neue Position laden
+                loadNextPosition(state);
+
+                // Status an der neuen Position laden
+                nextState = state[Xnext, Ynext];
+
+                if (stand)
+                    return;
+
+                // Wenn an der neuen Position ein Chip ist
+                switch (nextState)
+                {
+                    case eState.wood:
+
+                        if (hasChip)
+                        {
+                            this.setWood(state);    // Chip an aktueller Position ablegen
+                            countDown = 20;         // 20 Schritte machen bevor wieder was aufgenommen werden kann
+                            doStep(state);          // einen Schritt machen
+                        }
+                        else
+                        {
+                            pickWood(state);    // Chip aufnehmen und Status anpassen
+                            move(state);             // Position verändern
+
+                        }
+                        break;
+
+                    case eState.empty:
+                        if (state[X, Y] != eState.wood)
+                            state[X, Y] = eState.empty;     // Aktuelle Stelle auf leer setzen
+                        move(state);                         // in die Richtung gehen
+                        state[X, Y] = eState.termite;   // Neue Position setzen
+
+                        break;
+
+                    case eState.termite:
+                        break;
+                }
+            }
+
+        }
+        
+        #region IGuiExtension Members
+
+        public void AddMenuStripItems(MenuStrip menuStrip)
+        {
+            // Add items to the menu here
+        }
+
+        public void AddToolStripItems(ToolStrip toolStrip)
+        {
+            // Add items to the toolbar here
+        }
+
+        public void RemoveMenuStripItems(MenuStrip menuStrip)
+        {
+            // Remove all items from the menu
+        }
+
+        public void RemoveToolStripItems(ToolStrip toolStrip)
+        {
+            // Remove all items from the toolbar
+        }
+
+        #endregion
+
+        public override void Setup()
+        {
+            // Create a new raster image for drawing
+            _image = new Image<Rgb, byte>(ImageWidth, ImageHeight, new Rgb(Color.White));
+
+            // Create a new frame to display the raster image
+            _frame = new FrmImage(Name, ImageWidth + 1, ImageHeight + 1, DisplayMode.Zoomable)
+                {
+                    InterpolationMode = InterpolationMode.NearestNeighbor,
+                    SmoothingMode = SmoothingMode.None
+                };
+            _frame.SetImage(_image);
+
+            // Register this plugin as a GUI extension
+            GLabController.Instance.RegisterExtension(this);
+
+            // Stati initialisieren
+            init();
+
+            // Aufgabe starten
+            Aufgabe();
+        }
+
+        // wenn aktuelles feld weiß ist, dann rot färben - nach links drehen und eins vorwärts
+        // wenn aktuelles feld rot, dann weiß färben - nach rechts drehen und eins vorwärts
+        private void Aufgabe()
+        {
+            FrmFlowControl flow = new FrmFlowControl();
+            flow.StepCount = 1;
+            int i = 0;
+            int i2 = 0;
+            int iOut = 10000;
+
+            while (true)
+            {
+                i++;
+
+                //for (int ix = 0; ix < 100000; ix++) { }
+
+                // Alle Termiten eins weiter setzen
+                moveAllTermites();
+
+                if (i == iOut)
+                {
+                    // Statearray durchlaufen und Farben im Bild aktualisieren
+                    Logger.Instance.LogInfo("" + i2 + " * " + iOut);
+                    updateColors();
+                    i2++;
+                    i = 0;
+                    flow.Wait();    
+                }
+            }
+
+        }
+
+        // Alle Termiten einen Schritt ausführen lassen
+        private void moveAllTermites()
+        {
+            for (int i = 0; i < MAX_TERMITE; i++)
+            {
+                termites[i].doStep(state);
+            }
+        }
+
+        public override void Run()
+        {
+            // Executed in each iteration of the render loop
+        }
+
+        public override void Teardown()
+        {
+            GLabController.Instance.UnregisterExtension(this);
+            _frame.Dispose();
+            _image.Dispose();
+
+            // Additional teardown of your plugin
+            // ...
+        }
+
+        // aktualisiert das bild
+        // durchläuft das Statusarray und zeichnet die ensprechenden Pixel
+        // je nach Zustand
+        private void updateColors()
+        {
+            int x = 0;
+            int y = 0;
+
+            // Gesamtes Array auf empty setzen
+            for (x = 0; x < ImageWidth; x++)
+            {
+                for (y = 0; y < ImageHeight; y++)
+                {
+                    switch (state[x, y])
+                    {
+                        case eState.empty:
+                            _image[y, x] = new Rgb(Color.White);
+                            break;
+
+                        case eState.termite:
+                            _image[y, x] = new Rgb(Color.Black);
+                            break;
+
+                        case eState.wood:
+                            _image[y, x] = new Rgb(Color.Brown);
+                            break;
+                    }
+                }
+            }
+
+            _frame.Repaint();
+        }
+
+        // Imitalisiert das Array und setzt die Chips und die 
+        // Termiten an eine zufällige Stelle
+        private void init()
+        {
+            int x = 0;
+            int y = 0;
+
+            // Gesamtes Array auf empty setzen
+            for (x = 0; x < ImageWidth; x++)
+            {
+                for (y = 0; y < ImageHeight; y++)
+                {
+                    state[x, y] = eState.empty;
+                }
+            }
+
+            // Zufällig die Chips verteilen
+            for (int i = 0; i < MAX_WOOD; i++)
+            {
+                bool bSet = false;
+                do
+                {
+                    // Zufällige Position ermitteln
+                    x = _rand.Next() % ImageWidth;
+                    y = _rand.Next() % ImageHeight;
+
+                    // Wenn an der Position kein Chip liegt
+                    if (state[x, y] == eState.empty)
+                    {
+                        state[x, y] = eState.wood;
+                        bSet = true;
+                    }
+                } while (bSet == false);
+            }
+
+            // Termiten erzeugen
+            // Termiten erstellen
+            for (int i = 0; i < MAX_TERMITE; i++)
+            {
+                termites[i] = new Termite(state);
+            }
+        }
+    }
+}
